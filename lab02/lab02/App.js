668b66f128f1c063fc454031d1a58ea354fb2a4a
RyanVreeke <rvreeke0229@gmail.com>
2020-09-17 22:58:39 -0400

    Added lab02.

---
+++ lab02/lab02/App.js
@@ -0,0 +1,79 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
+
+export default function App() {
+  const [name, setName] = useState("Ryan");
+  const [age, setAge] = useState(0);
+  const [birthdays, setBirthdays] = useState([]);
+
+  const inputChange = (val) => {
+    setName(val);
+  };
+
+  const buttonPress = () => {
+    var newAge = age + 1;
+    setAge(newAge);
+    setBirthdays(birthdays.concat({ key: newAge.toString() }));
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text alignItems="center">Enter name:</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Input Name:"
+        onChangeText={inputChange}
+      />
+      <Text style={styles.textOuput}>
+        My name is {name} and I am {age} years old.
+      </Text>
+
+      <View styles={styles.buttonContainer}>
+        <Button title="BIRTHDAY" onPress={buttonPress} />
+      </View>
+      <FlatList
+        data={birthdays}
+        renderItem={({ item }) => <Text>{item.key}</Text>}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    padding: 30,
+    textAlignVertical: "top",
+  },
+
+  textInput: {
+    width: 135,
+    height: 35,
+    borderWidth: 2,
+    borderColor: "black",
+    margin: 8,
+    paddingLeft: 2,
+  },
+
+  textOuput: {
+    textAlign: "center",
+  },
+
+  buttonContainer: {
+    backgroundColor: "blue",
+    borderColor: "black",
+    borderWidth: 2,
+    color: "black",
+  },
+});
---
+++ lab02/lab02/README.md
@@ -0,0 +1,5 @@
+1. The name of the component in this application is App() where there are also subcomponents such as Veiw or Text or Button as well.
+2. One prop is setName or possibly onChangeText as well and one state element is name in the code.
+3. Curly braces {} are used to get the value of a state element or other element and use it however it is needed.
+4. There is an anonymous function in the code in inputChange(); where it constantly is changing the text being displayed for the name.
+5. No, this application when run on either Android or iOS, uses HTML and/or CSS but the react native framework does do something very similar.
