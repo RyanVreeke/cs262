adb1c060711aa120398ec94433197ea03deebf0c
RyanVreeke <rvreeke0229@gmail.com>
2020-10-15 22:22:02 -0400

    Finished the code for homework2 and started on lab07.

---
+++ homework2/gamezone/routes/aboutStack.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Header from "../shared/header";
+import About from "../screens/about";
+
+const Stack = createStackNavigator();
+
+export default function AboutStack({ navigation }) {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#ddd" },
+      }}
+    >
+      <Stack.Screen
+        name="About"
+        component={About}
+        options={{
+          headerLeft: () => <Header navigation={navigation} />,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
